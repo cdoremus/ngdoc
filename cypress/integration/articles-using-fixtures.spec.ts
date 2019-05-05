6fdb38de5cad785f03d2f2b269478ab1c3188c2a
Craig Doremus <cdoremus@gmail.com>
2019-05-05 15:40:57 -0400

    Implemented fixture test

--- cypress/integration/articles-using-fixtures.spec.ts
+++ cypress/integration/articles-using-fixtures.spec.ts
@@ -25,14 +25,28 @@ describe('Articles using fixtures', () => {
       cy.wait('@articles');
       cy.get('app-article').should('have.length', 3);
     });
+  });
 
     // USER CREATED TEST FOR TAGS
     // USE THE TAGS FIXTURE FILE TO STUB THE /api/tags call
     // WAIT FOR THE TAGS TO RETURN AND ASSERT THE COUNT
     // BONUS: Run tests using npx cypress run
     // BONUS: Figure out how to capture screenshots manually'
-    it('should filter tags', () => {
-      // write test here
+  // write test here
+  describe('should filter tags', () => {
+    beforeEach(() => {
+      cy.server();
+      cy.fixture('tags.json').as('tagsJSON');
+      cy.route('api/tags', '@tagsJSON').as('recent-tags');
+      cy.visit('/');
+      cy.wait('@recent-tags');
+    });
+    it('should have 4 tags with NgRx search', () => {
+      // use tagsJSON fixture
+      cy.route('GET', '/api/tags', '@tagsJSON');
+      // Enter 'NgRx' in the search box
+      cy.get('input#keyword').type('NgRx{enter}');
+      cy.get('div.tags').should('have.length', 4);
     });
   });
 });
